b735daf73249e6e320780e040885aa93ec3b2350
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-06-09 15:53:10 -0600

    Creamos las pantallas para las secciones de la aplicación. Falta redireccionar correctamente desde cada pantalla.

--- view/usuPerfil/index.php
+++ view/usuPerfil/index.php
@@ -1,1 +1,30 @@
-<h1>Perfil</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php require_once("../html/MainHead.php"); ?>
+    <title>Perfil</title>
+  </head>
+  <body>
+  <?php require_once("../html/MainMenu.php"); ?>
+  <?php require_once("../html/MainHeader.php"); ?>
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="index.html">Perfil</a>
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Perfil</h4>
+        <p class="mg-b-0">Pantalla Perfil</p>
+      </div>
+
+      <div class="br-pagebody">
+        <!-- start you own content here -->
+
+      </div><!-- br-pagebody -->
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+    <?php require_once("../html/MainJS.php"); ?>
+  </body>
+</html>
