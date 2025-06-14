5ac57bae9923c0015c7f6255f16ea8be2bf885c7
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-06-13 23:22:09 -0600

    Agregamos el backup de la base de datos del proyecto

---
+++ docs/diplomas_certificados.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 14-06-2025 a las 07:20:39
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `diplomas_certificados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `td_curso_usuario`
+--
+
+CREATE TABLE `td_curso_usuario` (
+  `curd_id` int(11) NOT NULL,
+  `cur_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `fecha_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) NOT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_curso`
+--
+
+CREATE TABLE `tm_curso` (
+  `cur_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `cur_nom` varchar(150) NOT NULL,
+  `cur_descrip` varchar(1000) NOT NULL,
+  `cur_fech_ini` date NOT NULL,
+  `cur_fech_fin` date NOT NULL,
+  `inst_id` int(11) NOT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_instructor`
+--
+
+CREATE TABLE `tm_instructor` (
+  `inst_id` int(11) NOT NULL,
+  `inst_nombre` varchar(150) NOT NULL,
+  `inst_apep` varchar(150) NOT NULL,
+  `inst_apem` varchar(150) NOT NULL,
+  `inst_correo` varchar(150) NOT NULL,
+  `inst_sex` varchar(1) NOT NULL,
+  `inst_telf` varchar(12) NOT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_usuario`
+--
+
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) NOT NULL,
+  `usu_apep` varchar(150) NOT NULL,
+  `usu_apem` varchar(150) NOT NULL,
+  `usu_correo` varchar(150) NOT NULL,
+  `usu_pass` varchar(150) NOT NULL,
+  `usu_sex` varchar(1) NOT NULL,
+  `usu_telf` varchar(12) NOT NULL,
+  `fech_crea` datetime DEFAULT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `td_curso_usuario`
+--
+ALTER TABLE `td_curso_usuario`
+  ADD PRIMARY KEY (`curd_id`);
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_curso`
+--
+ALTER TABLE `tm_curso`
+  ADD PRIMARY KEY (`cur_id`);
+
+--
+-- Indices de la tabla `tm_instructor`
+--
+ALTER TABLE `tm_instructor`
+  ADD PRIMARY KEY (`inst_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `td_curso_usuario`
+--
+ALTER TABLE `td_curso_usuario`
+  MODIFY `curd_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_curso`
+--
+ALTER TABLE `tm_curso`
+  MODIFY `cur_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_instructor`
+--
+ALTER TABLE `tm_instructor`
+  MODIFY `inst_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
