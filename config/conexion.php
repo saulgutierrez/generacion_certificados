5548e12af4f7f28806ea3c9875ff06893ed9003f
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-07-03 22:17:10 -0600

    Incluimos la conexion a base de datos

---
+++ config/conexion.php
@@ -0,0 +1,29 @@
+<?php
+    // Clase Conectar
+    class Conectar {
+        protected $dbh;
+
+        // Funcion protegida de la cadena de conexion
+        protected function Conexion() {
+            try {
+                // Cadena de conexion
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=diplomas_certificados","root","");
+                return $conectar;
+            } catch (Exception $e) {
+                // En caso de error en cadena de conexion
+                print "¡Error BD!: ". $e->getMessage() . "<br>";
+                die();
+            }
+        }
+
+        // Impedir que tengamos problemas con los tildes
+        public function set_names() {
+            return $this->dbh->query("SET NAMES 'utf-8'");
+        }
+
+        // Ruta principal del proyecto
+        public function ruta() {
+            return "http://localhostgeneracion_certificados/";
+        }
+    }
+?>
