eb45855d23c244bff3893ec34dbd56d65f6b25e4
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-08-19 22:44:26 -0600

    Creamos un mecanimo CRUD para las categorias a las que pertenecen los cursos. Estas podrán accederse desde un perfil de Administrador en el sistema.

--- controller/categoria.php
+++ controller/categoria.php
@@ -10,25 +10,20 @@
     switch($_GET["op"]) {
         // Guardar y editar cuando se tenga el id
         case "guardaryeditar":
-            if (empty($_POST["cur_id"])) {
-                $curso->insert_curso($_POST["cat_id"], $_POST["cur_nom"], $_POST["cur_descrip"], $_POST["cur_fech_ini"], $_POST["cur_fech_fin"], $_POST["inst_id"]);
+            if (empty($_POST["cat_id"])) {
+                $categoria->insert_categoria($_POST["cat_nom"]);
             } else {
-                $curso->update_curso($_POST["cat_id"], $_POST["cur_nom"], $_POST["cur_descrip"], $_POST["cur_fech_ini"], $_POST["cur_fech_fin"], $_POST["inst_id"], $_POST["cur_id"]);
+                $categoria->update_categoria($_POST["cat_id"], $_POST["cat_nom"]);
             }
             break;
         // Creando JSON segun el id
         case "mostrar":
-            $datos = $curso->get_curso_id($_POST["cur_id"]);
+            $datos = $categoria->get_categoria_id($_POST["cat_id"]);
             // Si la variable datos no esta vacia, y esta formateada en forma de array, la recorremos
             if (is_array($datos) == true and count($datos) <> 0) {
                 foreach ($datos as $row) {
-                    $output["cur_id"] = $row["cur_id"];
                     $output["cat_id"] = $row["cat_id"];
-                    $output["cur_nom"] = $row["cur_nom"];
-                    $output["cur_descrip"] = $row["cur_descrip"];
-                    $output["cur_fech_ini"] = $row["cur_fech_ini"];
-                    $output["cur_fech_fin"] = $row["cur_fech_fin"];
-                    $output["inst_id"] = $row["inst_id"];
+                    $output["cat_nom"] = $row["cat_nom"];
                 }
                 // Almacenamos los datos dentro de un array y lo convertimos a formato JSON, para que pueda ser leido por JS
                 echo json_encode($output);
@@ -36,21 +31,17 @@
             break;
         // Eliminar segun id
         case "eliminar":
-            $curso->delete_curso($_POST["cur_id"]);
+            $categoria->delete_categoria($_POST["cat_id"]);
             break;
         // Listar toda la informacion segun el formato de DataTable
         case "listar":
-            $datos = $curso->get_curso();
+            $datos = $categoria->get_categoria();
             $data = Array();
             foreach($datos as $row) {
                 $sub_array = array();
-                $sub_array[] = $row["cat_id"];
-                $sub_array[] = $row["cur_nom"];
-                $sub_array[] = $row["cur_fech_ini"];
-                $sub_array[] = $row["cur_fech_fin"];
-                $sub_array[] = $row["inst_id"];
-                $sub_array[] = '<button type="button" onClick="editar('.$row["cur_id"].');" id="'.$row["cur_id"].'" class="btn btn-outline-warning btn-icon"><div><i class="fa fa-edit"></i></div></button>';
-                $sub_array[] = '<button type="button" onClick="eliminar('.$row["cur_id"].');" id="'.$row["cur_id"].'" class="btn btn-outline-danger btn-icon"><div><i class="fa fa-close"></i></div></button>';
+                $sub_array[] = $row["cat_nom"];
+                $sub_array[] = '<button type="button" onClick="editar('.$row["cat_id"].');" id="'.$row["cat_id"].'" class="btn btn-outline-warning btn-icon"><div><i class="fa fa-edit"></i></div></button>';
+                $sub_array[] = '<button type="button" onClick="eliminar('.$row["cat_id"].');" id="'.$row["cat_id"].'" class="btn btn-outline-danger btn-icon"><div><i class="fa fa-close"></i></div></button>';
                 $data[] = $sub_array;
             }
 
--- models/Categoria.php
+++ models/Categoria.php
@@ -1,55 +1,44 @@
 <?php
     class Categoria extends Conectar {
-        public function insert_categoria($cat_id, $cur_nom, $cur_descrip, $cur_fech_ini, $cur_fech_fin, $inst_id) {
+        // Funcion para insertar categoria
+        public function insert_categoria($cat_nom) {
             $conectar = parent::Conexion();
             parent::set_names();
-            $sql = "INSERT INTO tm_curso (cur_id, cat_id, cur_nom, cur_descrip, cur_fech_ini, cur_fech_fin, inst_id, fech_crea, est) VALUES (NULL,?,?,?,?,?,?,now(),'1');";
+            $sql = "INSERT INTO tm_categoria (cat_id, cat_nom, fech_crea, est) VALUES (NULL, ?, now(), '1');";
             $sql = $conectar->prepare($sql);
-            $sql->bindValue(1, $cat_id);
-            $sql->bindValue(2, $cur_nom);
-            $sql->bindValue(3, $cur_descrip);
-            $sql->bindValue(4, $cur_fech_ini);
-            $sql->bindValue(5, $cur_fech_fin);
-            $sql->bindValue(6, $inst_id);
+            $sql->bindValue(1, $cat_nom);
             $sql->execute();
             return $resultado = $sql->fetchAll();
         }
-
-        public function update_categoria($cur_id,$cat_id, $cur_nom, $cur_descrip, $cur_fech_ini, $cur_fech_fin, $inst_id) {
+        
+        // Funcion para actualizar categoria
+        public function update_categoria($cat_id, $cat_nom) {
             $conectar = parent::Conexion();
             parent::set_names();
-            $sql = "UPDATE tm_curso
+            $sql = "UPDATE tm_categoria
                     SET
-                        cat_id = ?,
-                        cur_nom = ?,
-                        cur_descrip = ?,
-                        cur_fech_ini = ?,
-                        cur_fech_fin = ?,
-                        inst_id = ?
+                        cat_nom = ?
                     WHERE
-                        cur_id = ?";
+                        cat_id = ?";
             $sql = $conectar->prepare($sql);
-            $sql->bindValue(1, $cat_id);
-            $sql->bindValue(2, $cur_nom);
-            $sql->bindValue(3, $cur_descrip);
-            $sql->bindValue(4, $cur_fech_ini);
-            $sql->bindValue(5, $cur_fech_fin);
-            $sql->bindValue(6, $inst_id);
-            $sql->bindValue(7, $cur_id);
+            $sql->bindValue(1, $cat_nom);
+            $sql->bindValue(2, $cat_id);
             $sql->execute();
             return $resultado = $sql->fetchAll();
         }
 
-        public function delete_categoria($cur_id) {
+        // Eliminar/cambiar de estado la categoria
+        public function delete_categoria($cat_id) {
             $conectar = parent::Conexion();
             parent::set_names();
-            $sql = "UPDATE tm_curso SET est = 0 WHERE cur_id = ?";
+            $sql = "UPDATE tm_categoria SET est = 0 WHERE cat_id = ?";
             $sql = $conectar->prepare($sql);
-            $sql->bindValue(1, $cur_id);
+            $sql->bindValue(1, $cat_id);
             $sql->execute();
             return $resultado = $sql->fetchAll();
         }
 
+        // Listar todas las categorias
         public function get_categoria() {
             $conectar = parent::Conexion();
             parent::set_names();
@@ -59,12 +48,13 @@
             return $resultado = $sql->fetchAll();
         }
 
-        public function get_categoria_id($cur_id) {
+        // Filtrar segun id de categoria
+        public function get_categoria_id($cat_id) {
             $conectar = parent::Conexion();
             parent::set_names();
-            $sql = "SELECT * FROM tm_curso WHERE est = 1 AND cur_id = ?";
+            $sql = "SELECT * FROM tm_categoria WHERE cat_id = ?";
             $sql = $conectar->prepare($sql);
-            $sql->bindValue(1, $cur_id);
+            $sql->bindValue(1, $cat_id);
             $sql->execute();
             return $resultado = $sql->fetchAll();
         }
