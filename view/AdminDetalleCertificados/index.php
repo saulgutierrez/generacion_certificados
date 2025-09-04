880c6b24bc9849ebcc4833209b22507eb1ac963b
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-09-03 23:32:22 -0600

    Creamos la interfaz de la sección Detalle Certificado en la aplicación, la cual permitira añadir usuarios a los cursos, para que tengan acceso a sus certificados desde sus perfiles. Aún no finalizado.

---
+++ view/AdminDetalleCertificados/index.php
@@ -0,0 +1,81 @@
+<?php
+// Llamamos al archivo de conexion
+require_once("../../config/conexion.php");
+if (isset($_SESSION["usu_id"])) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <?php require_once("../html/MainHead.php"); ?>
+        <title>Empresa::Detalle Certificado</title>
+    </head>
+
+    <body>
+        <?php require_once("../html/MainMenu.php"); ?>
+        <?php require_once("../html/MainHeader.php"); ?>
+        <!-- ########## START: MAIN PANEL ########## -->
+        <div class="br-mainpanel">
+            <div class="br-pageheader pd-y-15 pd-l-20">
+                <nav class="breadcrumb pd-0 mg-0 tx-12">
+                    <a class="breadcrumb-item" href="#">Detalle Certificado</a>
+                </nav>
+            </div><!-- br-pageheader -->
+            <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+                <h4 class="tx-gray-800 mg-b-5">Detalle Certificado</h4>
+                <p class="mg-b-0">Mantenimiento</p>
+            </div>
+            <div class="br-pagebody">
+                <div class="br-section-wrapper">
+                    <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Detalle Certificado</h6>
+                    <p class="mg-b-30 tx-gray-600">Listado de Detalle Certificado</p>
+                    <div class="form-layout">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                    <div class="form-group">
+                                        <label class="form-control-label">Cursos: <span class="tx-danger">*</span> </label>
+                                        <select class="form-control select2" style="width: 100%" data-placeholder="Seleccione" name="usu_sex" id="usu_sex">
+                                            <option label="Seleccione"></option>
+                                            <option value="M">Masculino</option>
+                                            <option value="F">Femenino</option>
+                                        </select>
+                                    </div>
+                            </div><!-- col-4 -->
+                            <div class="col-lg-4">
+                                    <label class="form-control-label">&nbsp;</label>
+                                    <button class="btn btn-outline-primary form-control" id="add_button" onclick="nuevo()"><i class="fa fa-plus-square mg-r-10"></i> Agregar usuarios</button>
+                            </div><!-- col-4 -->
+                        </div><!-- row -->
+                    </div><!-- form-layout -->
+                    <p></p>
+                    <div class="table-wrapper">
+                        <table id="detalle_data" class="table display responsive nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="wd-15p">Curso</th>
+                                    <th class="wd-15p">Fecha Inicio</th>
+                                    <th class="wd-20p">Fecha Fin</th>
+                                    <th class="wd-15p">Instructor</th>
+                                    <th class="wd-10p"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div><!-- br-section-wrapper -->
+            </div><!-- br-pagebody -->
+        </div><!-- br-mainpanel -->
+        <!-- ########## END: MAIN PANEL ########## -->
+        <?php require_once("modalmantenimiento.php"); ?> 
+        <?php require_once("../html/MainJS.php"); ?>
+        <script type="text/javascript" src="adminmdetallecertificado.js"></script>
+    </body>
+
+    </html>
+<?php
+} else {
+    // Si no ha iniciado sesion, se redirecciona a la ventana principal
+    header("Location: " . Conectar::ruta() . "view/404");
+}
+?>
