2637b104a1a670fdcf89a11ff744aa242f2ae281
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-10-10 20:57:37 -0600

    Podemos cargar un archivo de Excel (xlsx) en el sistema y darle lectura a su contenido. Aún no finalizado.

--- view/AdminMntUsuario/adminmntusuario.js
+++ view/AdminMntUsuario/adminmntusuario.js
@@ -144,4 +144,49 @@ $(document).on("click", "#btnplantilla", function () {
     $("#modalplantilla").modal("show");
 });
 
+var ExcelToJSON = function() {
+
+    this.parseExcel = function(file) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+            // Hace un recorrido por todas las pestañas/hojas del excel
+            workbook.SheetNames.forEach(function(sheetName) {
+                // Here is your object
+                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                var json_object = JSON.stringify(XL_row_object);
+                UserList = JSON.parse(json_object);
+
+                console.log(UserList)
+                for (i = 0; i < UserList.length; i++) {
+
+                    var columns = Object.values(UserList[i])
+                }
+
+            })
+        };
+        reader.onerror = function(ex) {
+            console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+
+
+
+    };
+};
+
+function handleFileSelect(evt) {
+
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();
+    xl2json.parseExcel(files[0]);
+}
+
+document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+
 init();
